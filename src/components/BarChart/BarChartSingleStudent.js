7369c107e7edce5c541932134eb2cb06c788c2fb
Hans Kool <Hansjekool@hotmail.com>
2021-01-22 14:03:40 +0100

    simple single bar

--- src/components/BarChart/BarChartSingleStudent.js
+++ src/components/BarChart/BarChartSingleStudent.js
@@ -19,66 +19,69 @@ class BarChartSingleStudent extends React.Component {
     const studentName = getSingleStudentData[0].Name;
 
     return (
-      <VictoryChart
-        height={500}
-        width={2000}
-        theme={VictoryTheme.material}
-        domainPadding={20}
-        padding={{ left: 50, right: 30, top: 80, bottom: 150 }}
-      >
-        <VictoryAxis
-          label={"Opdrachten " + studentName}
-          fixLabelOverlap={true}
-          style={{
-            grid: { strokeDasharray: "none", strokeWidth: 0 },
-            axisLabel: { padding: 50, fill: "gold" },
-            tickLabels: {
-              padding: 1,
-              angle: -45,
-              verticalAnchor: "middle",
-              textAnchor: "end",
-              fill: "white",
-              y: 10,
-            },
-          }}
-        />
-        <VictoryAxis
-          dependentAxis
-          label={"Beoordeling"}
-          style={{
-            axisLabel: { fill: "gold" },
-            tickLabels: { fill: "white", padding: 20 },
-          }}
-        />
-        <VictoryGroup offset={10} colorScale={"qualitative"}>
-          <VictoryBar
-            // barRatio={1}
-            // domainPadding={0.1}
-            // animate={{
-            //   duration: 2000,
-            //   onLoad: { duration: 1000 },
-            // }}
-            barWidth={12}
-            cornerRadius={{ topLeft: 3, topRight: 3 }}
-            data={getSingleStudentData}
-            x="Project"
-            y="Difficulty"
+      <div>
+        <h4 className="titleSingleStudentBar" >{studentName}</h4>
+        <VictoryChart
+          height={500}
+          width={2000}
+          theme={VictoryTheme.material}
+          domainPadding={20}
+          padding={{ left: 50, right: 30, top: 80, bottom: 150 }}
+        >
+          <VictoryAxis
+            label={"Opdrachten " + studentName}
+            fixLabelOverlap={true}
+            style={{
+              grid: { strokeDasharray: "none", strokeWidth: 0 },
+              axisLabel: { padding: 50, fill: "gold" },
+              tickLabels: {
+                padding: 1,
+                angle: -45,
+                verticalAnchor: "middle",
+                textAnchor: "end",
+                fill: "white",
+                y: 10,
+              },
+            }}
           />
-          <VictoryBar
-            // barRatio={1}
-            // animate={{
-            //   duration: 2000,
-            //   onLoad: { duration: 2000 },
-            // }}
-            barWidth={12}
-            cornerRadius={{ topLeft: 3, topRight: 3 }}
-            // data={findAverage}
-            data={getSingleStudentData}
-            x="Project"
-            y="Rating"
+          <VictoryAxis
+            dependentAxis
+            label={"Beoordeling"}
+            style={{
+              axisLabel: { fill: "gold" },
+              tickLabels: { fill: "white", padding: 20 },
+            }}
           />
-        </VictoryGroup>
-      </VictoryChart>
+          <VictoryGroup offset={10} colorScale={"qualitative"}>
+            <VictoryBar
+              // barRatio={1}
+              // domainPadding={0.1}
+              // animate={{
+              //   duration: 2000,
+              //   onLoad: { duration: 1000 },
+              // }}
+              barWidth={12}
+              cornerRadius={{ topLeft: 3, topRight: 3 }}
+              data={getSingleStudentData}
+              x="Project"
+              y="Difficulty"
+            />
+            <VictoryBar
+              // barRatio={1}
+              // animate={{
+              //   duration: 2000,
+              //   onLoad: { duration: 2000 },
+              // }}
+              barWidth={12}
+              cornerRadius={{ topLeft: 3, topRight: 3 }}
+              // data={findAverage}
+              data={getSingleStudentData}
+              x="Project"
+              y="Rating"
+            />
+          </VictoryGroup>
+        </VictoryChart>
+      </div>
     );
   }
 }
